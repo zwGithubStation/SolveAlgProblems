36a1f19998b5d36253fc094760ee07e4022ed6c0
zwGithubStation <zwp_sz_pro@163.com>
2020-02-24 15:19:10 +0800

    basic alg

--- Chpater_1/Festival/ngx_array.c
+++ Chpater_1/Festival/ngx_array.c
@@ -6,29 +6,75 @@
 
 #include <stdio.h>
 
+int get_min_total(int cnt, int max, int *prices)
+{
+	int i,j,cur,answer = 0;
+	for (i = 0; i <= max-cnt; i++)
+	{
+		cur = 0;
+		for (j = i; j < i+cnt; j++)
+		{
+			cur += prices[j];
+		}
+
+		if (cur < answer)
+			answer = cur;
+	}
+
+	return answer;
+}
+
+float get_answer(int max, int min, int *prices)
+{
+	int i,total;
+	float answer = 0.0;
+	float cur = 0.0;
+	for (i = min; i <= max; i++)
+	{
+		total = get_min_total(i, max, prices);
+		cur = (float)total / (float)i;
+		if (cur < answer)
+			answer = cur;
+	}
+
+	return answer;
+}
+
 int main()
 {
 	int c,n,l,i,j;
+	int range[100][2] = {0};
 	int price[100][1000] = {0};
+	float output = 0.0;
 	scanf("%d", &c);
-	for(i = 0 ; i < c ; i++)
+	for(i = 0 ; i < c; i++)
 	{
-		scanf("%d %d", &n, &l);
+		scanf("%d %d", &(range[i][0]), &(range[i][1]));
 		for(j = 0; j < n; j++)
 		{
 			scanf("%d",&(price[i][j]));
 		}
 	}
 
-	/*test*/
-	for(i = 0 ; i < 100 ; i++)
+	for(i = 0 ; i < c; i++)
 	{
-		for(j = 0; j < 1000; j++)
+		output = get_answer(range[i][0], range[i][1], &(price[i][0]));
+		printf("%.10f", output);
+		if(i != c-1)
 		{
-			printf("%d ",price[i][j]);
+			printf("\n");
 		}
-		printf("\n");
 	}
+
+	/*test*/
+//	for(i = 0 ; i < 100 ; i++)
+//	{
+//		for(j = 0; j < 1000; j++)
+//		{
+//			printf("%d ",price[i][j]);
+//		}
+//		printf("\n");
+//	}
 	
 	return 0;
 }
